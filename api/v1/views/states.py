3a4e44e60d1b2058715eb78906efef5a21727e0b
bustlezach <bustlezach001@gmail.com>
2023-07-31 12:53:23 -0700

    task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 This script creates a new view for State objects that handles all default RESTFul API actions:
 
@@ -17,29 +17,24 @@ def get_states():
     """retrieve the list of all states or post new one"""
     if request.method == 'GET':
         states = storage.all(State)
-        state_list = []
-        for state in states.values():
-            state_list.append(state.to_dict())
-            return jsonify(state_list)
+        state_list = [state.to_dict() for state in states.values()]
+        return jsonify(state_list)
     elif request.method == 'POST':
         data = request.get_json()
-        if type(data) is not dict:
+        if not isinstance(data, dict):
             return make_response(jsonify({"error": "Not a JSON"}), 400)
-        if data.get('name') is None:
+        if 'name' not in data:
             return make_response(jsonify({"error": "Missing name"}), 400)
         new_state = State(**data)
         new_state.save()
         return jsonify(new_state.to_dict()), 201
-    abort(404)
 
 
 @app_views.route('/states/<string:state_id>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
 def alt_states(state_id):
     """alter the values of state"""
-    if state_id is None:
-        abort(404)
     state = storage.get(State, state_id)
-    if state_id not in state.keys():
+    if state is None:
         abort(404)
     if request.method == 'GET':
         return jsonify(state.to_dict())
@@ -49,10 +44,10 @@ def alt_states(state_id):
         return jsonify({}), 200
     elif request.method == 'PUT':
         data = request.get_json()
-        if type(data) is not dict:
+        if not isinstance(data, dict):
             return make_response(jsonify({"error": "Not a JSON"}), 400)
         for k, v in data.items():
             if k not in ['id', 'created_at', 'updated_at']:
                 setattr(state, k, v)
-                storage.save()
-        return jsonify(state.to_dict()), 201
+        storage.save()
+        return jsonify(state.to_dict()), 200
