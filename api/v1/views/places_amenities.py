c25f8f7018fe73907fc183d805d575f8292d6f1a
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-31 05:57:04 +0000

    test

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -12,12 +12,19 @@ def get_place_amenities(place_id):
     place = storage.get('Place', place_id)
     if not place:
         abort(404)
-    return jsonify([storage_t == "db" and amenity.to_dict() or amenity.id for
-                    amenity in place.amenities])
+    amenities = []
+    if storage_t == 'db':
+        amenities = [amenity.to_dict() for amenity in place.amenities]
+    else:
+        for id in place.amenity_ids:
+            amenity = storage.get('Amenity', id)
+            if amenity:
+                amenities.append(amenity.to_dict())
+    return jsonify(amenities)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE'], strict_slashes=False)
+@ app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                  methods=['DELETE'], strict_slashes=False)
 def delete_place_amenity(place_id, amenity_id):
     """Takes a place_id and amenity_id and remove/delete depending on storage
     engine
@@ -30,16 +37,16 @@ def delete_place_amenity(place_id, amenity_id):
         abort(404)
     if storage_t == 'db' and amenity in place.amenities:
         place.amenities.remove(amenity)
-    elif storage_t != 'db' and amenity.id in place.amenities:
-        place.amenities.remove(amenity.id)
+    elif storage_t != 'db' and amenity.id in place.amenity_ids:
+        place.amenity_ids.remove(amenity.id)
     else:
         abort(404)
     storage.save()
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['POST'], strict_slashes=False)
+@ app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                  methods=['POST'], strict_slashes=False)
 def post_place_amenity(place_id, amenity_id):
     """Attach a amenity to a place"""
     place = storage.get('Place', place_id)
@@ -50,13 +57,11 @@ def post_place_amenity(place_id, amenity_id):
         abort(404)
     if storage_t == 'db' and amenity in place.amenities:
         return make_response(jsonify(amenity), 200)
-    elif storage_t == 'db' and amenity.id in place.amenities:
+    elif storage_t == 'db' and amenity.id in place.amenity_ids:
         return make_response(jsonify(amenity), 200)
     if storage_t == 'db':
         place.amenities.append(amenity)
     else:
-        place.amenities.append(amenity.id)
-        storage.save()
-        return make_response(jsonify(amenity.id), 201)
+        place.amenity_ids.append(amenity.id)
     storage.save()
     return make_response(jsonify(amenity.to_dict()), 201)
